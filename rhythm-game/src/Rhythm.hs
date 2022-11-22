9f077f103af96214c0625f60a800ccd97ee4769a
lilianjiw107 <lilianjiw107@gmail.com>
2022-11-22 00:00:03 -0800

    Basic gaming UI drawn

--- rhythm-game/src/Rhythm.hs
+++ rhythm-game/src/Rhythm.hs
@@ -1,4 +1,5 @@
 module Rhythm where
+import Control.Lens hiding ((<|), (|>), (:>), (:<), Empty)
 
 --- Game definitions: --
 data Note = N { height :: Int }
@@ -9,11 +10,11 @@ data HitState
     | Good
     | Miss
     | Empty
-    deriving (Eq, Ord)
+    deriving (Show, Eq, Ord)
 
 -- Game State:
 data Game = Game
-  { _song       :: [[Note]]
+  { _song       :: [[Int]]
   , _score      :: Int
   , _lastHit    :: HitState
   , _done       :: Bool
@@ -24,14 +25,19 @@ data Game = Game
 initGame :: IO Game
 initGame = do
   pure $
-    Game { _song = [[N 10, N 20, N 30, N 40],
-                    [N 5, N 20, N 25, N 30],
-                    [N 15, N 40],
-                    [N 30, N 50, N 60, N 90]]
+    Game { _song = [[10, 20, 30, 40],
+                    [5, 20, 25, 30],
+                    [15, 40],
+                    [30, 50, 60, 90]]
         , _lastHit = Empty
         , _score = 0
         , _done = False
         }
 
 step :: Game -> Game
-step g = g
+step g = Game
+    { _song       = map (map (\h -> h-1)) (_song g)
+    , _score      = (_score g)
+    , _lastHit    = (_lastHit g)
+    , _done       = (_done g)
+    } 
--- rhythm-game/src/UI.hs
+++ rhythm-game/src/UI.hs
@@ -29,6 +29,8 @@ import qualified Data.Sequence as S
 -- marks passing of time
 data Tick = Tick
 
+data Col = FirstCol | SecondCol | ThirdCol | FourthCol | EmptyCol | Bottom
+
 type Name = ()
 
 -- define App
@@ -60,15 +62,15 @@ handleEvent g (AppEvent Tick)                       = continue $ step g
 -- Drawing
 drawUI :: Game -> [Widget Name]
 drawUI g =
-  [ C.center $ padRight (Pad 4) (drawStats g) <+> drawGrid g <+> padLeft (Pad 4) drawLastHit]
+  [ C.center $ padRight (Pad 4) (drawStats g) <+> drawGrid g <+> padLeft (Pad 4) (drawLastHit g)]
 
-drawLastHit :: Widget Name
-drawLastHit = withBorderStyle BS.unicodeBold
-  $ hLimit 5
+drawLastHit :: Game -> Widget Name
+drawLastHit g = withBorderStyle BS.unicodeBold
+  $ hLimit 20
   $ B.borderWithLabel (str "Last Hit")
   $ C.hCenter
   $ padAll 1
-  $ str "Perfect"
+  $ str $ show (_lastHit g)
 
 drawStats :: Game -> Widget Name
 drawStats g = hLimit 11
@@ -84,10 +86,45 @@ drawScore n = withBorderStyle BS.unicodeBold
 drawGrid :: Game -> Widget Name
 drawGrid g = withBorderStyle BS.unicodeBold
   $ B.borderWithLabel (str "Rhythm")
-  $ C.hCenter
-  $ padAll 1
-  $ str $ show 1
+  $ vBox rows
+  where
+    rows         = [hBox $ cellsInRow r | r <- [39, 38..0]]
+    cellsInRow y = [(drawCoord x y) | x <- [0..40]]
+    drawCoord x y
+        | y == 1                      = drawCell Bottom
+        | x >= 1 && x < 9 && y `elem` (_song g)!!0       = drawCell FirstCol
+        | x >= 11 && x < 19 && y `elem` (_song g)!!1     = drawCell SecondCol
+        | x >= 21 && x < 29 && y `elem` (_song g)!!2     = drawCell ThirdCol
+        | x >= 31 && x < 39 && y `elem` (_song g)!!3     = drawCell FourthCol
+        | otherwise             = drawCell EmptyCol
+
+drawCell :: Col -> Widget Name
+drawCell FirstCol = withAttr firstAttr cw
+drawCell SecondCol  = withAttr secondAttr cw
+drawCell ThirdCol = withAttr thirdAttr cw
+drawCell FourthCol = withAttr fourthAttr cw
+drawCell EmptyCol = withAttr emptyAttr cw
+drawCell Bottom = withAttr bottomAttr cwBottom
+
+cw :: Widget Name
+cw = str "  "
+
+cwBottom :: Widget Name
+cwBottom = str "--"
 
 theMap :: AttrMap
 theMap = attrMap V.defAttr
-  []
+  [ (firstAttr, V.blue `on` V.blue)
+  , (secondAttr, V.red `on` V.red)
+  , (thirdAttr, V.green `on` V.green)
+  , (fourthAttr, V.yellow `on` V.yellow)
+  , (bottomAttr, V.cyan `on` V.black)
+  ]
+
+firstAttr, secondAttr, thirdAttr, fourthAttr :: AttrName
+firstAttr = attrName "firstAttr"
+secondAttr = attrName "secondAttr"
+thirdAttr = attrName "thirdAttr"
+fourthAttr = attrName "fourthAttr"
+emptyAttr = attrName "emptyAttr"
+bottomAttr = attrName "bottomAttr"
