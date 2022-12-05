24bdc240c1b709eeed993a4977f5bcb7e4ba4c99
vagrant <vagrant@cse230>
2022-12-05 02:05:46 +0000

    Using monad to achieve game loops

--- rhythm-game/src/Main.hs
+++ rhythm-game/src/Main.hs
@@ -5,16 +5,10 @@ module Main
 import UI (mainGame)
 import DeveloperUI (mainDeveloper)
 import ChooseMode (chooseMode)
-import Control.Monad (void,when)
-
-repeatNTimes 0 _ = return ()
-repeatNTimes n action =
- do
-  action
-  repeatNTimes (n-1) action
+import Control.Monad (void,when,replicateM_)
 
 main :: IO ()
-main = repeatNTimes 10 (do
+main = replicateM_ 10 (do
     mode <- chooseMode
     Control.Monad.when (mode == 1) $ void mainGame
     Control.Monad.when (mode == 2) $ void mainDeveloper)
